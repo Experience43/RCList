9ea6906249217a3895a6fc28c7a5b64cbbf2dde9
Experience43 <131533879+Experience43@users.noreply.github.com>
2026-09-25 08:44:24 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,13 +1,13 @@
 // https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
-// export function getYoutubeIdFromUrl(url) {
-//     return url.match(
-//         /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/,
-//     )?.[1] ?? '';
-// }
+export function getYoutubeIdFromUrl(url) {
+    return url.match(
+        /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/,
+    )?.[1] ?? '';
+}
 
-// export function embed(video) {
-//     return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(video)}`;
-// }
+export function embed(video) {
+    return `https://www.youtube.com/embed/${getYoutubeIdFromUrl(video)}`;
+}
 
 //test code to see if it works
 export function embed(video) {
